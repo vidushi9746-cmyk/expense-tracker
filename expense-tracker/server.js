0e3e01ed99c6e80ba655d9b8e9375e7ffa05722a
vidushi9746-cmyk <vidushi.9746@gmail.com>
2026-05-15 23:54:23 +0530

    added cors

--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -1,12 +1,14 @@
 const express= require('express');
 const dotenv= require('dotenv');
 const connectDB= require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app= express();
 app.use(express.json());
+app.use(cors());
 
 const authRoutes= require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
