16fe0fb4c4106b06425e3498865496eb1378ee25
vidushi9746-cmyk <vidushi.9746@gmail.com>
2026-05-16 00:22:14 +0530

    fixed route order

--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -2,23 +2,17 @@ const express= require('express');
 const dotenv= require('dotenv');
 const connectDB= require('./config/db');
 const cors = require('cors');
-
 dotenv.config();
 connectDB();
-
 const app= express();
 app.use(express.json());
 app.use(cors());
-
 const authRoutes= require('./routes/authRoutes');
+const expenseRoutes = require('./routes/expenseRoutes');
 app.use('/api/auth', authRoutes);
-
+app.use('/api/expenses', expenseRoutes);
 app.get('/',(req,res) =>{
     res.json({message: 'Expense Tracker API is running '});
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const expenseRoutes = require('./routes/expenseRoutes');
-app.use('/api/expenses', expenseRoutes);
